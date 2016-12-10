b651a28046a57c0649bbf18a5f4bab422954f862
ornab <hzamil17@gmail.com>
2016-12-10 14:16:10 +0600

    modified

--- inc/custom-posts.php
+++ inc/custom-posts.php
@@ -1,7 +1,7 @@
 
  <?php      
 
-function Theme_E_custom_posts(){
+function theme_e_custom_posts(){
         register_post_type( 'featured work',
             array(
                 'labels' => array(
@@ -12,6 +12,20 @@ function Theme_E_custom_posts(){
                 'public' => true
             )
         );
+    
+          register_post_type( 'slide',
+            array(
+                'label'=> 'Slides',
+                'labels' => array(
+                    'name' => __( 'Slides' ),
+                    'singular_name' => __( 'Slide' )
+                ),
+                'menu_icon'=>'dashicons-images-alt',
+                'supports' => array('title', 'editor', 'custom-fields', 'thumbnail', 'page-attributes', 'excerpt'),
+                'public' => true
+            )
+        );
+       
     }
 
-add_action( 'init', 'Theme_E_custom_posts' );
+add_action( 'init', 'theme_e_custom_posts' );
--- templates/slider.php
+++ templates/slider.php
@@ -11,22 +11,49 @@
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
+    
+     <?php
+global $post;
+$args = array( 'posts_per_page' => 10, 'post_type'=> 'custom-post-type' );
+$myposts = get_posts( $args );
+foreach( $myposts as $post ) : setup_postdata($post); ?>
+
+    <?php 
+        $job_link= get_post_meta($post->ID, 'job_instructions', true); 
+    ?>
+
+	
+	
+	
+	
+	
+	
     <div class="item active">
       <img src="<?php  echo get_template_directory_uri(); ?>/img/slide1.jpg" alt="">
       <div class="slider-caption">
-		<h2>WELCOME TO OUR LOGIC
-</h2>
+		<h2><?php the_title(); ?></h2>
+		
+		 <p><?php the_excerpt(); ?></p>
+		 
+		 <p><?php echo $job_link; ?></p>
+		
+		
+		
+		
+		<!-- 
 		<p>We Make Awesome Theme For Your Business</p>
+       -->
       </div>
     </div>
-    <div class="item">
-      <img src="<?php  echo get_template_directory_uri(); ?>/img/slide2.jpg" alt="">
-      <div class="slider-caption">
-		<h2>WELCOME TO OUR LOGIC</h2>
-		<p>We Make Awesome Theme For Your Business</p>
-      </div>
-    </div>
+    
   </div>
+	
+	
+<?php endforeach; ?>
+   
+   
+   
+   
 
   <!-- Controls -->
   <a class="left slider-left-btn" href="#carousel-example-generic" role="button" data-slide="prev">
